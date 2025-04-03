602b902ce4e050f3bf71458a308ee8391c19f266
youn <tty7797@naver.com>
2025-04-03 09:37:55 +0900

    환경설정

---
+++ src/test/java/CommunityChat/CommunityChatApplicationTests.java
@@ -0,0 +1,13 @@
+package CommunityChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
